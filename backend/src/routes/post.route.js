21555c57ba0ba331aa1e2f9627688453ad57fa73
melomatthewdev <devscustomers@outlook.com>
2025-10-06 21:42:54 -0500

    small fixes

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { createPost, deletePost, getPost, getPosts, getUserPosts, likePost } from "../controllers/post.controller.js";
+import { 
+    createPost, 
+    deletePost, 
+    getPost, 
+    getPosts, 
+    getUserPosts, 
+    likePost 
+} from "../controllers/post.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import upload from "../middleware/upload.middleware.js";
+
 
 const router = express.Router();
 
